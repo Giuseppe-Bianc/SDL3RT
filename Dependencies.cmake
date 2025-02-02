6bd7fec4b3492ab7b4aaa2c5704a23edbc83f5ae
GIUSEPPE BIANCONI <gbianconi3@gmail.com>
2025-02-02 22:02:01 +0100

    Aggiorna dipendenze e sostituisce ftxui con SDL
    Sono state apportate modifiche al file `Dependencies.cmake` per aggiornare le versioni delle dipendenze e aggiungere la funzione `AddSpdlogPackage` per configurare `spdlog` con supporto per `wchar` e `wchar_filenames` in base al sistema operativo. Nel file `CPM.cmake`, la versione di `CPM_DOWNLOAD_VERSION` è stata aggiornata da `0.38.1` a `0.40.5`. Nel file `CMakeLists.txt`, sono stati rimossi i target `lefticus::tools` e `ftxui` e aggiunto il target `SDL3::SDL3`. Nel file `main.cpp`, sono stati rimossi numerosi include e codice relativi a `ftxui` e `lefticus::tools`, sostituendoli con codice per inizializzare e gestire una finestra e un renderer SDL, con un ciclo di eventi SDL per gestire l'uscita del programma. La funzione `main` è stata modificata per rimuovere le chiamate alle funzioni `consequence_game` e `game_iteration_canvas`, sostituendole con l'inizializzazione di SDL, la creazione di una finestra e un renderer, e un ciclo di eventi per gestire l'uscita del programma.

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -1,5 +1,21 @@
 include(cmake/CPM.cmake)
 
+function(AddSpdlogPackage WcharSupport WcharFilenames)
+  CPMAddPackage(
+          NAME spdlog
+          VERSION 1.15.1
+          GITHUB_REPOSITORY "gabime/spdlog"
+          OPTIONS
+          "SPDLOG_FMT_EXTERNAL ON"
+          "SPDLOG_ENABLE_PCH ON"
+          "SPDLOG_BUILD_PIC ON"
+          "SPDLOG_WCHAR_SUPPORT ${WcharSupport}"
+          "SPDLOG_WCHAR_FILENAMES ${WcharFilenames}"
+          "SPDLOG_SANITIZE_ADDRESS OFF"
+  )
+endfunction()
+
+
 # Done as a function so that updates to variables like
 # CMAKE_CXX_FLAGS don't propagate out to other
 # targets
@@ -8,36 +24,38 @@ function(SDL3RT_setup_dependencies)
   # For each dependency, see if it's
   # already been provided to us by a parent project
 
-  if(NOT TARGET fmtlib::fmtlib)
-    cpmaddpackage("gh:fmtlib/fmt#9.1.0")
-  endif()
+  if (NOT TARGET fmtlib::fmtlib)
+    CPMAddPackage("gh:fmtlib/fmt#11.1.3")
+  endif ()
 
-  if(NOT TARGET spdlog::spdlog)
-    cpmaddpackage(
-      NAME
-      spdlog
-      VERSION
-      1.11.0
-      GITHUB_REPOSITORY
-      "gabime/spdlog"
-      OPTIONS
-      "SPDLOG_FMT_EXTERNAL ON")
-  endif()
+  if (NOT TARGET spdlog::spdlog)
+    if (WIN32)
+      AddSpdlogPackage(ON ON)
+    else ()
+      AddSpdlogPackage(OFF OFF)
+    endif ()
+  endif ()
 
-  if(NOT TARGET Catch2::Catch2WithMain)
-    cpmaddpackage("gh:catchorg/Catch2@3.3.2")
-  endif()
+  if (NOT TARGET Catch2::Catch2WithMain)
+    CPMAddPackage("gh:catchorg/Catch2@3.8.0")
+  endif ()
 
-  if(NOT TARGET CLI11::CLI11)
-    cpmaddpackage("gh:CLIUtils/CLI11@2.3.2")
-  endif()
+  if (NOT TARGET CLI11::CLI11)
+    CPMAddPackage("gh:CLIUtils/CLI11@2.4.2")
+  endif ()
 
-  if(NOT TARGET ftxui::screen)
-    cpmaddpackage("gh:ArthurSonzogni/FTXUI@5.0.0")
+  if(NOT TARGET SDL3::SDL3)
+    CPMAddPackage(
+            NAME SDL3
+            GITHUB_REPOSITORY libsdl-org/SDL
+            GIT_TAG release-3.2.2
+            OPTIONS
+            "SDL_SHARED OFF"
+            "SDL_STATIC ON"
+            "SDL_TEST_LIBRARY OFF"
+            #"SDL_INSTALL_TESTS OFF"
+            #"SDL_DISABLE_INSTALL_DOCS OFF"
+            #"SDL_DISABLE_INSTALL OFF"
+    )
   endif()
-
-  if(NOT TARGET tools::tools)
-    cpmaddpackage("gh:lefticus/tools#update_build_system")
-  endif()
-
 endfunction()
